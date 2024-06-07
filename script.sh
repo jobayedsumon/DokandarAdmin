871e72af3b123305cb239da660e4bda46ee770ff
Jobayed Sumon <jobayedahmedsumon@gmail.com>
2024-06-07 16:31:51 +0600

    cpanel yaml updated and script updated

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-(crontab -l | grep -v "/usr/bin/php /home/956826.cloudwaysapps.com/pcenmpwtgr/public_html/artisan dm:disbursement") | crontab -
-(crontab -l ; echo "57 13 * * * /usr/bin/php /home/956826.cloudwaysapps.com/pcenmpwtgr/public_html/artisan dm:disbursement") | crontab -
-(crontab -l | grep -v "/usr/bin/php /home/956826.cloudwaysapps.com/pcenmpwtgr/public_html/artisan store:disbursement") | crontab -
-(crontab -l ; echo "57 13 * * * /usr/bin/php /home/956826.cloudwaysapps.com/pcenmpwtgr/public_html/artisan store:disbursement") | crontab -
+(crontab -l | grep -v "/usr/bin/php /home/dokanda1/admin.dokandar.online/artisan dm:disbursement") | crontab -
+(crontab -l ; echo "57 13 * * * /usr/bin/php /home/dokanda1/admin.dokandar.online/artisan dm:disbursement") | crontab -
+(crontab -l | grep -v "/usr/bin/php /home/dokanda1/admin.dokandar.online/artisan store:disbursement") | crontab -
+(crontab -l ; echo "57 13 * * * /usr/bin/php /home/dokanda1/admin.dokandar.online/artisan store:disbursement") | crontab -
+(crontab -l | grep -v "/usr/bin/php /home/dokanda1/admin.dokandar.online/artisan redeem:package") | crontab -
+(crontab -l ; echo "0 0 * * * /usr/bin/php /home/dokanda1/admin.dokandar.online/artisan redeem:package") | crontab -
